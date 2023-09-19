3087d4e6d591f958f0f98e396f1563b216d86825
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-09-19 20:40:38 +0200

    Pexels-Clone
    - Implementato la ricerca delle query in modo corretto: adesso l'input ricerca delle foto in base a ciò che viene inserito nella casella;
    
    -Implementato del "mini css" per dare un transform alle carte che appaiono dopo la ricerca;

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 // FUNZIONE PER RICHIAMARE L'API E FAR APPARIRE LE FOTO NELLA PAGINA COME CARD
 const url = "https://api.pexels.com/v1/search?query="
 
-const getPhotos = (query) => {
+const ricerca = document.querySelector("#ricerca")
 
-    fetch(url + query, {
-        headers: {
-            Authorization: "ov8uL2jcCXt25GvOv7oMlt03RLy3xj9UndWMqixkRcdZu6z76Sb0DpVI",
-        },
-    })
-        .then(response => response.json())
-        .then(result => {
-            let contenuto = document.querySelector(".album .row")
-            contenuto.innerHTML = result.photos.map((photo) => {
-                // QUI ANDRO' A CREARE LE MIE CARD 
-                return `
-                <div class="col col-3">
-                    <div class="card">
-                        <img src='${photo.src.medium}' class="card-img-top" />
+
+const getPhotos = () => {
+
+
+    if (ricerca.value !== "") {
+        fetch(url + ricerca.value, {
+            headers: {
+                Authorization: "ov8uL2jcCXt25GvOv7oMlt03RLy3xj9UndWMqixkRcdZu6z76Sb0DpVI",
+            },
+        })
+            .then(response => response.json())
+            .then(result => {
+                let contenuto = document.querySelector(".album .row")
+                contenuto.innerHTML = result.photos.map((photo) => {
+                    // QUI ANDRO' A CREARE LE MIE CARD 
+                    return `
+                <div class="col col-4">
+                    <div class="card mb-4">
+                        <img src='${photo.src.large}' class="card-img-top img-fluid" />
                         <div class="card-body">
-                            <h5>${photo.alt}</h5>
-                            <p class="card-text">Photographer: '${photo.photographer}'</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h5>${photo.alt}</h5>
+                                <p class="card-text">Photographer: '${photo.photographer}'</p>
+                            </div>
                         </div>
                     </div>
                 </div>`
-            }).join("")
-        })
-        .catch(error => console.log('error', error));
+                }).join("")
+            })
+            .catch(error => console.log('error', error));
+    }
+    else {
+        contenuto.innerHTML = "";
+    }
 }
